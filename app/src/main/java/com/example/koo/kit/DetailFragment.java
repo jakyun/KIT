7dc083186c105d91e4bee5e6c31e7a23593dde18
jakyun <olrw02@naver.com>
2017-07-10 17:47:03 +0900

    DetailFragment Test

--- app/src/main/java/com/example/koo/kit/DetailFragment.java
+++ app/src/main/java/com/example/koo/kit/DetailFragment.java
@@ -17,6 +17,7 @@ public class DetailFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    //test
 
     private String mParam1;
     private String mParam2;
